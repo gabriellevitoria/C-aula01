ef0d1d73cc2f2e52bf1e944aad2fd9a1f1831eae
Gabrielle Vitoria <gabriellev912@gmail.com>
2023-08-15 17:50:21 -0300

    aula01

---
+++ Pessoa.cs
@@ -0,0 +1,21 @@
+class Pessoa
+{
+    //atributos:
+    
+    public string nome;
+
+    public int idade;
+
+    public float altura;
+
+    //construtor:
+    public Pessoa(string nome) {
+        this.nome =  nome;
+    }
+
+    public Pessoa (string nome, int idade) {
+        this.nome = nome;
+        this.idade = idade; 
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using inteiro = System.Int32;
+Int32 xpto = 5;
+inteiro y = inteiro.Parse("3");
+
+
+Console.WriteLine("Hello, World!");
+
+Pessoa maria = new Pessoa("Maria");
+Console.WriteLine(maria.nome);
+Console.WriteLine(maria.idade);
+
+Pessoa bruce = new Pessoa("Bruce", 49);
+Console.WriteLine(bruce.nome);
+Console.WriteLine(bruce.idade);
+
+
